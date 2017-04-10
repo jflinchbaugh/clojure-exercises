95123d4188637651e516e03d75414f6290e4abb2
John Flinchbaugh <john@hjsoft.com>
2017-04-10 07:19:20 -0400

    comment on scoring example

--- scoring/this-town.clj
+++ scoring/this-town.clj
@@ -1,3 +1,7 @@
+(comment
+    scoring for the game.
+    https://boardgamegeekstore.com/products/tmg-micro-game-this-town-aint-big-enough
+)
 (def scores {:red 12, :blue 3, :yellow 1})
 
 (defn sort-map [scores] (reverse (sort-by val scores)))
