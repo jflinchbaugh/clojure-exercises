8ba2a1aa4179086b41afeb80ad19c364ab1d95c3
John Flinchbaugh <john@hjsoft.com>
2017-04-10 07:36:55 -0400

    tests for scoring

--- scoring/this-town.clj
+++ scoring/this-town.clj
@@ -7,9 +7,13 @@
     https://boardgamegeekstore.com/products/tmg-micro-game-this-town-aint-big-enough
 )
 
+(use '[clojure.test :as t])
+(ns this-town)
+
 (defn sort-map [scores] (reverse (sort-by val scores)))
 
-(defn score [counts]
+(defn score
+    [counts]
     (let [ordered (sort-map counts)]
         ; each player gets the next count that's less their own
         (zipmap
@@ -20,3 +24,14 @@
 )
 
 (score {:red 12, :blue 1, :yellow 1, :green 4})
+
+(t/deftest score-tests
+    (is
+        (=
+            (score {:red 12, :blue 1, :yellow 1, :green 4})
+            {:red 4, :green 1}
+        )
+    )
+)
+
+(t/run-tests)
